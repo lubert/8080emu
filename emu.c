e7b373a474b5394af09fb8378f603dfaa01be5a2
lubert <misterzhu@gmail.com>
2019-04-23 13:09:18 -0700

    Implement some POP and PUSH

--- emu.c
+++ emu.c
@@ -122,6 +122,15 @@ int Emulate8080Op(State8080* state) {
       state->a = answer & 0xff;
     }
     break;
+  case 0xc1:
+    // POP B
+    // C <- (sp); B <- (sp + 1); sp <- sp + 2
+    {
+      state->c = state->memory[state->sp];
+      state->b = state->memory[state->sp + 1];
+      state->sp += 2;
+    }
+    break;
   case 0xc2:
     // JNZ addr
     if (0 == state->cc.z) // "Not-Z"
@@ -132,6 +141,15 @@ int Emulate8080Op(State8080* state) {
   case 0xc3:
     // JMP addr
     state->pc = opcode[2] << 8 | opcode[1];
+    break;
+  case 0xc5:
+    // PUSH B
+    // (sp - 2) <- C; (sp - 1) <- B; sp <- sp - 2
+    {
+      state->memory[state->sp - 2] = state->c;
+      state->memory[state->sp - 1] = state->b;
+      state->sp -= 2;
+    }
     break;
   case 0xc6:
     // ADI D8 "Immediate Form"
@@ -177,6 +195,36 @@ int Emulate8080Op(State8080* state) {
       state->cc.cy = 0;
       state->a = x;
       state->pc += 1;
+    }
+    break;
+  case 0xf1:
+    // POP PSW
+    // flags <- (sp); A <- (sp + 1); sp <- sp + 2
+    {
+      state->a = state->memory[state->sp+1];
+      // "Program Status Word"
+      // Saves the flags using a bit array
+      uint8_t psw = state->memory[state->sp];
+      state->cc.z  = (0x01 == (psw & 0x01));
+      state->cc.s  = (0x02 == (psw & 0x02));
+      state->cc.p  = (0x04 == (psw & 0x04));
+      state->cc.cy = (0x08 == (psw & 0x08));
+      state->cc.ac = (0x10 == (psw & 0x10));
+      state->sp += 2;
+    }
+    break;
+  case 0xf5:
+    // PUSH PSW
+    // (sp - 2) <- flags; (sp - 1) <- A; sp <- sp - 2
+    {
+      state->memory[state->sp-1] = state->a;
+      uint8_t psw = (state->cc.z |
+                     state->cc.s  << 1 |
+                     state->cc.p  << 2 |
+                     state->cc.cy << 3 |
+                     state->cc.ac << 4 );
+      state->memory[state->sp-2] = psw;
+      state->sp -= 2;
     }
     break;
   case 0xfe:
